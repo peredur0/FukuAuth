55f6142bca5ce753d8640868dd7a48688a6e0083
peredur0 <martial.goehry@gmail.com>
2025-08-06 11:47:58 +0200

    docs: manual version update 2

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -12,7 +12,6 @@ NO UPDATE: build, chore, docs, refactor, test
 """
 
 import re
-import sys
 import pathlib
 import logging
 import subprocess
@@ -101,8 +100,6 @@ def main() -> None:
         )
     else:
         logger.info("Version not updated - %s", current_version)
-
-    sys.exit(0)
 
 
 if __name__ == "__main__":
